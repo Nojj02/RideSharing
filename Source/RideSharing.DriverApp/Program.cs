dbbd4f0d374009611ed720c0626b9b582fb08fd8
Jonn <nojj02@gmail.com>
2018-03-28 21:08:24 +0800

    Added 3 more projects

---
+++ Source/RideSharing.DriverApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RideSharing.DriverApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
